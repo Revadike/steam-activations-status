70ea3509af5e3954866e10822e6574169eb343ff
Royalgamer06 <royalgamer06@hotmail.com>
2017-05-07 01:52:09 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
-npm install steamauth
-npm install steam-user
-npm install steamcommunity
-npm install fs
+CD %~dp0
+start npm install steamauth
+start npm install steam-user
+start npm install steamcommunity
+start npm install fs
 pause
